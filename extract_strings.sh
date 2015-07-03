498c0f53be5345319e63b1ac2ccd420bb089fe46
AhmadHamzeei <ahmadhamzeei@gmail.com>
2015-07-03 22:02:13 +0430

    update extract_string.sh

--- extract_strings.sh
+++ extract_strings.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xgettext -k_ -kN_ -o po/amir.pot amir/*.py amir/database/*.py bin/amir data/ui/*.glade
+xgettext -k_ -kN_ -o po/amir.pot amir/*.py amir/database/*.py bin/amir.py data/ui/*.glade
+msgmerge po/fa.po po/amir.pot -o po/fa.po
 
-#msgfmt po/*.po 
